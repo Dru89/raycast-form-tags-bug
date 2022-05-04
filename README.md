6f87c156f5b24b9b734bc3328be2c0b81825dd81
Drew Hays <drewshays@gmail.com>
2022-05-04 11:17:09 -0700

    initial commit with bug

---
+++ README.md
@@ -0,0 +1,15 @@
+# Form.TagPicker Bug
+
+Simple example for showing a bug in Form.TagPicker
+
+![Form Empty State](./screenshots/empty-state.png)
+
+## Bug Description
+
+Setup: `npm install && npm run dev`
+
+Steps to reproduce: Run "Form Bug" command, observe `Changed` detail view.
+
+Expected: No fields should be marked as `changed`.
+
+Actual: `tags` is marked as changed (from `[]` to `[]`).
---
+++ src/index.tsx
@@ -0,0 +1,71 @@
+import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { useReducer, useState } from "react";
+import dedent from "ts-dedent";
+
+type FormAction = { field: "tags"; value: string[] } | { field: "text"; value: string };
+
+interface FormState {
+  tags: string[];
+  text: string;
+}
+
+function reducer(state: FormState, action: FormAction): FormState {
+  switch (action.field) {
+    case "tags":
+      return { ...state, tags: action.value };
+    case "text":
+      return { ...state, text: action.value };
+    default:
+      throw new Error(`Unknown action: ${JSON.stringify(action)}`);
+  }
+}
+
+export default function Command() {
+  const [details, setDetails] = useState<string[]>([]);
+  const [state, dispatch] = useReducer(reducer, { tags: [], text: "" });
+
+  const append = (message: string) => {
+    setDetails((details) => [...details, message]);
+  };
+
+  const handleChange = (action: FormAction) => {
+    append(`${action.field} changed: ${JSON.stringify(action.value)}, was: ${JSON.stringify(state[action.field])}`);
+    dispatch(action);
+  };
+
+  return (
+    <Form navigationTitle="Form Bug">
+      <Form.Description
+        title="Bug Description"
+        text={dedent`
+        Steps to Reproduce: Load form, look at "Changed"
+
+        Expected: No changes should be received yet.
+
+        Action: \`tags\` changed (from \`[]\` to \`[]\`)
+      `}
+      />
+      <Form.TextField
+        title="Text Field"
+        id="text"
+        value={state.text}
+        onChange={(value) => handleChange({ field: "text", value })}
+      />
+      <Form.TagPicker
+        title="🌈 Tags"
+        id="tags"
+        value={state.tags}
+        onChange={(value) => handleChange({ field: "tags", value })}
+      >
+        <Form.TagPicker.Item title="Red" value="red" />
+        <Form.TagPicker.Item title="Orange" value="orange" />
+        <Form.TagPicker.Item title="Yellow" value="yellow" />
+        <Form.TagPicker.Item title="Green" value="green" />
+        <Form.TagPicker.Item title="Blue" value="blue" />
+        <Form.TagPicker.Item title="Indigo" value="indigo" />
+        <Form.TagPicker.Item title="Violet" value="violet" />
+      </Form.TagPicker>
+      <Form.Description title="Form Changes" text={details.slice(-5).join("\n")} />
+    </Form>
+  );
+}
